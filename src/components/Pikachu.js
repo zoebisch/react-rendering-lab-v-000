b9a4ff5f1c700e5c95ffba6966381aa926eb03a7
Brian Zoebisch <zoebisch@gmail.com>
2018-05-12 19:57:30 -0400

    Done.

--- src/components/Pikachu.js
+++ src/components/Pikachu.js
@@ -9,6 +9,10 @@ class Pikachu extends React.Component {
       size: 100
     };
   }
+
+  componentDidUpdate() {
+    this.resizePikachu()
+  }
   
   resizePikachu = () => {
     const pikachu = document.getElementById("pikachu");
